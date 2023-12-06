d188c65a63d1c25f2d64973d1317984871890419
haydenleung <90701608+hayden1126@users.noreply.github.com>
2023-12-06 11:23:02 +0000

    optimised code

--- day6.jl
+++ day6.jl
@@ -1,39 +1,28 @@
 
 function day6_1(file::String)::Nothing
     times, rec_dists = readlines(file)
-    races = zip(Int32[parse(Int32, n) for n = split(times)[2:end]], 
-                Int32[parse(Int32, n) for n = split(rec_dists)[2:end]])
-    sum = 1
-
-    for (timelimit, rec_dist) = races
-        bounds = Int32[timelimit÷2, timelimit÷2 + 1] # number of milliseconds charging
-        while (timelimit-bounds[1])*bounds[1] > rec_dist
-            bounds[1] -= 1
-        end
-        while (timelimit-bounds[2])*bounds[2] > rec_dist
-            bounds[2] += 1
-        end
-        sum *= (bounds[2] - bounds[1] - 1)
-    end
-
-    println(sum)
+    races = zip(Int[parse(Int, n) for n = split(times)[2:end]], 
+                Int[parse(Int, n) for n = split(rec_dists)[2:end]])
+    println(mapreduce(get_range, *, races))
 end
 
 function day6_2(file::String)::Nothing
     times, rec_dists = readlines(file)
     timelimit = parse(Int, join(split(times)[2:end]))
-    rec_dist = parse(Int, join(split(rec_dists)[2:end]))
+    rec_dist = parse(Int, join(split(rec_dists)[2:end]))    
+    println(get_range((timelimit, rec_dist)))
+end
 
-    bounds = Int32[timelimit÷2, timelimit÷2 + 1] # number of milliseconds charging 
-    while (timelimit-bounds[1])*bounds[1] > rec_dist
-        bounds[1] -= 1
+function get_range(race::Tuple{Int64, Int64})::Int
+    (timelimit, rec_dist) = race
+    a, b = timelimit÷2, timelimit÷2 + 1
+    while (timelimit-a)*a > rec_dist
+        a -= 1
     end
-    while (timelimit-bounds[2])*bounds[2] > rec_dist
-        bounds[2] += 1
+    while (timelimit-b)*b > rec_dist
+        b += 1
     end
-
-    sum = (bounds[2] - bounds[1] - 1)
-    println(sum)
+    return (b - a - 1)
 end
 
 file = "inputs/day6.txt"
